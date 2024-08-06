77dfc8af660559698ed028cb39a83c56a4450848
KellerJordan <kellerjordan1998@gmail.com>
2024-08-06 16:24:06 +0000

    .

--- clean_airbench.py
+++ clean_airbench.py
@@ -1,9 +1,11 @@
 """
-clean_airbench.py
+cleaner_airbench.py
 
 Variant of airbench94 which removes the following:
 * Lookahead optimization
 * Progressive freezing
+
++imports the dataloader.
 
 Attains 93.95 mean accuracy.
 """
@@ -23,7 +25,7 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as T
 
-from airbench import evaluate
+from airbench import evaluate, CifarLoader
 
 torch.backends.cudnn.benchmark = True
 
@@ -64,109 +66,6 @@ hyp = {
         'tta_level': 2,
     },
 }
-
-
-#############################################
-#                DataLoader                 #
-#############################################
-
-CIFAR_MEAN = torch.tensor((0.4914, 0.4822, 0.4465))
-CIFAR_STD = torch.tensor((0.2470, 0.2435, 0.2616))
-
-def batch_flip_lr(inputs):
-    flip_mask = (torch.rand(len(inputs), device=inputs.device) < 0.5).view(-1, 1, 1, 1)
-    return torch.where(flip_mask, inputs.flip(-1), inputs)
-
-def batch_crop(images, crop_size):
-    r = (images.size(-1) - crop_size)//2
-    shifts = torch.randint(-r, r+1, size=(len(images), 2), device=images.device)
-    images_out = torch.empty((len(images), 3, crop_size, crop_size), device=images.device, dtype=images.dtype)
-    # The two cropping methods in this if-else produce equivalent results, but the second is faster for r > 2.
-    if r <= 2:
-        for sy in range(-r, r+1):
-            for sx in range(-r, r+1):
-                mask = (shifts[:, 0] == sy) & (shifts[:, 1] == sx)
-                images_out[mask] = images[mask, :, r+sy:r+sy+crop_size, r+sx:r+sx+crop_size]
-    else:
-        images_tmp = torch.empty((len(images), 3, crop_size, crop_size+2*r), device=images.device, dtype=images.dtype)
-        for s in range(-r, r+1):
-            mask = (shifts[:, 0] == s)
-            images_tmp[mask] = images[mask, :, r+s:r+s+crop_size, :]
-        for s in range(-r, r+1):
-            mask = (shifts[:, 1] == s)
-            images_out[mask] = images_tmp[mask, :, :, r+s:r+s+crop_size]
-    return images_out
-
-class CifarLoader:
-
-    def __init__(self, path, train=True, batch_size=500, aug=None, drop_last=None, shuffle=None, altflip=False):
-
-        data_path = os.path.join(path, 'train.pt' if train else 'test.pt')
-        if not os.path.exists(data_path):
-            dset = torchvision.datasets.CIFAR10(path, download=True, train=train)
-            images = torch.tensor(dset.data)
-            labels = torch.tensor(dset.targets)
-            torch.save({'images': images, 'labels': labels, 'classes': dset.classes}, data_path)
-        data = torch.load(data_path, map_location='cuda')
-
-        self.epoch = 0
-        self.images, self.labels, self.classes = data['images'], data['labels'], data['classes']
-        # It's faster to load+process uint8 data than to load preprocessed fp16 data
-        self.images = (self.images.half() / 255).permute(0, 3, 1, 2).to(memory_format=torch.channels_last)
-
-        self.normalize = T.Normalize(CIFAR_MEAN, CIFAR_STD)
-        self.proc_images = {} # Saved results of image processing to be done on the first epoch
-
-        self.aug = aug or {}
-        for k in self.aug.keys():
-            assert k in ['flip', 'translate'], 'Unrecognized key: %s' % k
-
-        self.batch_size = batch_size
-        self.drop_last = train if drop_last is None else drop_last
-        self.shuffle = train if shuffle is None else shuffle
-        self.altflip = altflip
-
-    def __len__(self):
-        return len(self.images)//self.batch_size if self.drop_last else ceil(len(self.images)/self.batch_size)
-    
-    def __setattr__(self, k, v):
-        if k in ('images', 'labels'):
-            assert self.epoch == 0, 'Changing images or labels is only unsupported before iteration.'
-        super().__setattr__(k, v)
-
-    def __iter__(self):
-
-        if self.epoch == 0:
-            images = self.proc_images['norm'] = self.normalize(self.images)
-            # Pre-flip images in order to do every-other epoch flipping scheme
-            if self.aug.get('flip', False):
-                images = self.proc_images['flip'] = batch_flip_lr(images)
-            # Pre-pad images to save time when doing random translation
-            pad = self.aug.get('translate', 0)
-            if pad > 0:
-                self.proc_images['pad'] = F.pad(images, (pad,)*4, 'reflect')
-
-        if self.aug.get('translate', 0) > 0:
-            images = batch_crop(self.proc_images['pad'], self.images.shape[-2])
-        elif self.aug.get('flip', False):
-            images = self.proc_images['flip']
-        else:
-            images = self.proc_images['norm']
-        # Flip all images together every other epoch. This increases diversity relative to random flipping
-        if self.aug.get('flip', False):
-            if self.altflip:
-                if self.epoch % 2 == 1:
-                    images = images.flip(-1)
-            else:
-                images = batch_flip_lr(images)
-
-        self.epoch += 1
-
-        indices = (torch.randperm if self.shuffle else torch.arange)(len(images), device=images.device)
-        for i in range(len(self)):
-            idxs = indices[i*self.batch_size:(i+1)*self.batch_size]
-            yield (images[idxs], self.labels[idxs])
-
 
 #############################################
 #            Network Components             #
