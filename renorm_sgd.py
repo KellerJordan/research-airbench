adf46f0c2005127d2092df0246ae5aac3e4f63dd
KellerJordan <kellerjordan1998@gmail.com>
2024-08-11 23:02:35 +0000

    .

--- renorm_sgd.py
+++ renorm_sgd.py
@@ -1,92 +1,56 @@
 import torch
 from torch import Tensor
-from torch.optim.optimizer import (Optimizer, _use_grad_for_differentiable)
+from torch.optim.optimizer import Optimizer
 from typing import List, Optional
 
 class RenormSGD(Optimizer):
-    def __init__(self, params, lr=1e-3, momentum=0, dampening=0,
-                 nesterov=False, *, maximize: bool = False, differentiable: bool = False):
+    def __init__(self, params, lr=1e-3, momentum=0, dampening=0, nesterov=False):
         if lr < 0.0:
             raise ValueError(f"Invalid learning rate: {lr}")
         if momentum < 0.0:
             raise ValueError(f"Invalid momentum value: {momentum}")
-
-        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
-                        nesterov=nesterov,
-                        maximize=maximize, differentiable=differentiable)
         if nesterov and (momentum <= 0 or dampening != 0):
             raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
+                        nesterov=nesterov)
         super().__init__(params, defaults)
 
-    def __setstate__(self, state):
-        super().__setstate__(state)
-        for group in self.param_groups:
-            group.setdefault('nesterov', False)
-            group.setdefault('maximize', False)
-            group.setdefault('differentiable', False)
-
-    def _init_group(self, group, params_with_grad, d_p_list, momentum_buffer_list):
-        for p in group['params']:
-            if p.grad is not None:
-                params_with_grad.append(p)
-                d_p_list.append(p.grad)
-                state = self.state[p]
-                momentum_buffer_list.append(state.get('momentum_buffer'))
-
-    @_use_grad_for_differentiable
     def step(self, closure=None):
-        """Performs a single optimization step.
-
-        Args:
-            closure (Callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
         loss = None
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
 
         for group in self.param_groups:
-            params_with_grad = []
-            d_p_list = []
-            momentum_buffer_list = []
-
-            self._init_group(group, params_with_grad, d_p_list, momentum_buffer_list)
+            params_with_grad = [p for p in group['params'] if p.grad is not None]
+            d_p_list = [p.grad for p in params_with_grad]
+            momentum_buffer_list = [self.state[p].get('momentum_buffer') for p in params_with_grad]
 
             renorm_sgd(params_with_grad,
-                d_p_list,
-                momentum_buffer_list,
-                momentum=group['momentum'],
-                lr=group['lr'],
-                dampening=group['dampening'],
-                nesterov=group['nesterov'],
-                maximize=group['maximize'])
+                       d_p_list,
+                       momentum_buffer_list,
+                       momentum=group['momentum'],
+                       lr=group['lr'],
+                       dampening=group['dampening'],
+                       nesterov=group['nesterov'])
 
             # update momentum_buffers in state
             for p, momentum_buffer in zip(params_with_grad, momentum_buffer_list):
-                state = self.state[p]
-                state['momentum_buffer'] = momentum_buffer
+                self.state[p]['momentum_buffer'] = momentum_buffer
 
         return loss
 
 def renorm_sgd(params: List[Tensor],
-        d_p_list: List[Tensor],
-        momentum_buffer_list: List[Optional[Tensor]],
-        # kwonly args with defaults are not supported by functions compiled with torchscript issue #70627
-        # setting this as kwarg for now as functional API is compiled by torch/distributed/optim
-        *,
-        momentum: float,
-        lr: float,
-        dampening: float,
-        nesterov: bool,
-        maximize: bool):
-    r"""Functional API that performs SGD algorithm computation.
-
-    See :class:`~torch.optim.SGD` for details.
-    """
+               d_p_list: List[Tensor],
+               momentum_buffer_list: List[Optional[Tensor]],
+               *,
+               momentum: float,
+               lr: float,
+               dampening: float,
+               nesterov: bool):
 
     for i, param in enumerate(params):
-        d_p = d_p_list[i] if not maximize else -d_p_list[i]
+        d_p = d_p_list[i]
 
         if momentum != 0:
             buf = momentum_buffer_list[i]
@@ -105,8 +69,6 @@ def renorm_sgd(params: List[Tensor],
         #shape = [len(d_p)]+[1]*(len(d_p.shape)-1)
         #d_p_hat = d_p / d_p.reshape(len(d_p), -1).norm(dim=1).view(*shape)
         #param.div_(param.data.reshape(len(param), -1).norm(dim=1).view(*shape))
-        d_p_hat = d_p / d_p.norm()
-        param.div_(param.data.norm())
-
-        param.add_(d_p_hat, alpha=-lr)
+        param.data.div_(param.data.norm())
+        param.data.add_(d_p, alpha=-lr / d_p.norm())
 
