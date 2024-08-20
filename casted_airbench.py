9e88fdba54d4bfa80ca3fa75d2e3d0239bafe7d4
KellerJordan <kellerjordan1998@gmail.com>
2024-08-20 08:45:56 +0000

    .

--- casted_airbench.py
+++ casted_airbench.py
@@ -5,7 +5,7 @@ Accuracy: 93.95 in n=50
 
 At various precisions:
 * bits=3 -> 93.95 (n=50)
-* bits=2 -> 93.95 (n=50)
+* bits=2 -> 93.95 (n=100)
 * bits=1 -> 93.86 (n=50)
 * bits=0 -> 93.67 (n=50)
 
@@ -24,6 +24,11 @@ What if we leave the weights 2-bit, and also cast the inputs?
 * bits=4 -> 93.89 (n=50)
 * bits=3 -> 93.82 (n=50)
 * bits=2 -> 93.63 (n=50)
+
+What if we don't upper-bound the first weight (which is the whitening layer)?
+* (lg a, lg b) = (-inf, 1) -> 93.90 (n=20)
+* (lg a, lg b) = (-inf, 0) -> 93.98 (n=20)
+* (lg a, lg b) = (-inf, -1) -> 93.89 (n=20)
 
 """
 
@@ -65,7 +70,7 @@ hyp = {
         'tta_level': 2,
         'conv_precision': {
             'bits': 2, # bits per binary level: bits=3 means we can represent 8 values in the range [1, 2), 8 values in [2, 4) etc
-            'upper_bound': 128,
+            'upper_bound': 1/2,
             'lower_bound': 0,
         }
     },
@@ -276,5 +281,5 @@ if __name__ == '__main__':
 
     print(evaluate(train(train_loader), test_loader, tta_level=hyp['net']['tta_level']))
     print(torch.std_mean(torch.tensor([evaluate(train(train_loader), test_loader, tta_level=hyp['net']['tta_level'])
-                                       for _ in range(50)])))
+                                       for _ in range(20)])))
 
