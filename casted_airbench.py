b79805bc998fa48e8beef5544d1861b5228ce851
KellerJordan <kellerjordan1998@gmail.com>
2024-08-20 08:54:47 +0000

    .

--- casted_airbench.py
+++ casted_airbench.py
@@ -28,7 +28,7 @@ What if we leave the weights 2-bit, and also cast the inputs?
 What if we don't upper-bound the first weight (which is the whitening layer)?
 * (lg a, lg b) = (-inf, 1) -> 93.90 (n=20)
 * (lg a, lg b) = (-inf, 0) -> 93.98 (n=20)
-* (lg a, lg b) = (-inf, -1) -> 93.89 (n=20)
+* (lg a, lg b) = (-inf, -1) -> 93.95 (n=50)
 * (lg a, lg b) = (-inf, -2) -> 93.73 (n=10)
 * (lg a, lg b) = (-inf, -3) -> 93.58 (n=10)
 
@@ -72,7 +72,7 @@ hyp = {
         'tta_level': 2,
         'conv_precision': {
             'bits': 2, # bits per binary level: bits=3 means we can represent 8 values in the range [1, 2), 8 values in [2, 4) etc
-            'upper_bound': 1,
+            'upper_bound': 2**0,
             'lower_bound': 2**-8,
         }
     },
