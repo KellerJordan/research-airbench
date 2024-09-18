9e6a34348c3c3b1ce0fcc1c3078bcbf8145bdafd
KellerJordan <kellerjordan1998@gmail.com>
2024-09-18 01:29:55 +0000

    .

---
+++ run.sh
@@ -0,0 +1,11 @@
+pip install airbench
+p=sweep_casted.py
+CUDA_VISIBLE_DEVICES=0 python $p
+CUDA_VISIBLE_DEVICES=1 python $p
+CUDA_VISIBLE_DEVICES=2 python $p
+CUDA_VISIBLE_DEVICES=3 python $p
+CUDA_VISIBLE_DEVICES=4 python $p
+CUDA_VISIBLE_DEVICES=5 python $p
+CUDA_VISIBLE_DEVICES=6 python $p
+CUDA_VISIBLE_DEVICES=7 python $p
+
